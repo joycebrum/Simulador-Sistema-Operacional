2d5d75df4155c17bea5d898d36bb158e6431a57d
Lucas Rampazzo <lucas.rampazzo@visagio.com>
2019-04-28 00:15:33 -0300

    Modificando processos.c para processos.h

---
+++ Simulador/processos.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "variables.h"
+
+int PID = 10;
+
+Processo createNewProcess(int priority, int PPID) {
+	Processo newProcesso;
+
+	newProcesso.priority = priority;
+	newProcesso.PID = PID++;
+	newProcesso.PPID = PPID;
+	newProcesso.status = created;
+	newProcesso.waitTime = 0;
+
+	return newProcesso;
+}
+
+void increaseWaitTimeProcess(Processo *processo, float timePassed) {
+	processo->waitTime += timePassed;
+}
+
+void resetWaitTimeProcess(Processo *processo) {
+	processo->waitTime = 0;
+}
+
+void blockProcess(Processo *processo) {
+	//TODO adiciona na fila de blocked e remove da fila de running
+	processo->status = blocked;
+}
+
+void runProcess(Processo *processo) {
+	//TODO remove da fila de waiting
+	processo->status = running;
+}
+void endProcess(Processo *processo) {
+	//TODO remove o processo da lista de processos ativos
+	processo->status = terminated;
+}
+void interruptProcess(Processo *processo) {
+	processo->status = waiting;
+}
+void unblockProcess(Processo *processo) {
+	//TODO remove o processo da fila de bloqueados
+	processo->status = waiting;
+}
