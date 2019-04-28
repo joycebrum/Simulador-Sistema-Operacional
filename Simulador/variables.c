34938f9ed7711355312566cd2942ec493ac78436
Lucas Rampazzo <lucas.rampazzo@visagio.com>
2019-04-27 21:04:54 -0300

    Ajustes struct + nome variáveis

--- Simulador/variables.c
+++ Simulador/variables.c
@@ -1,12 +1,12 @@
-#define NUMERO_MAXIMO_PROCESSOS_CRIADOS 20;
-#define FATIA_TEMPO_POR_PROCESSO 4;
+#define MAX_PROCESSOS 20;
+#define TEMPO_RR 4;
 #define DISCO 0;
 #define FITA_MAGNETICA 1;
 #define IMPRESSORA 2;
 
-typedef struct {
+typedef struct _IO {
 	int tipoIO;
-	float tempo;
+	double tempo;
 } IO;
 
 IO tiposIO[3] = [{ .tipoIO = 0, .tempo = 15}, { .tipoIO = 1, .tempo = 30}, { .tipoIO = 2, .tempo = 20}]
