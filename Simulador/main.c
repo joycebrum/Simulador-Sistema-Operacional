4c6854ab1aaa0b2c98931957ac1d032013d015ec
Lucas Rampazzo <lucas.rampazzo@visagio.com>
2019-05-03 21:24:08 -0300

    Encapsulando escalonarProcesso e mudanças adicionais

--- Simulador/main.c
+++ Simulador/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include "variables.h"
 #include "processos.h"
 #include "fifo.h"
 #include "gerenciadorfilas.h"
 
-int tempoDecorrido;
-int tempoExecutando;
-int tempoGeracaoProcessos[MAX_PROCESSOS];
-int proximoProcessoASerCriado = 0;
+/*-Global Variables---------------------------------------------------*/
+int tempoDecorrido;//Variável que representa o tempo 
+int tempoExecutando;//
+int tempoGeracaoProcessos[MAX_PROCESSOS];//Vetor com 20 tempos de chegada de processos aleatórios
+int numProcesso; //Índice do último processo criado
 Processo *processoExecutando;
-bool processadorExecutando;
 int tempoExecutadoProcessador;
 
 void inicializacao();
@@ -18,70 +19,68 @@ void executarProcesso();
 void geraTempoAleatorioParaCriacaoProcessos();
 void escalonarProcesso();
 void processador();
-void criaProcessosSeHouverDemanda();
+void criaProcessos();
 
+/*-Main---------------------------------------------------------------*/
 int main () {
 	inicializacao();
 	geraTempoAleatorioParaCriacaoProcessos();
-	while(1) {
-
-		updateBlockedProcesses();
-		
-		criaProcessosSeHouverDemanda();
-
-		processador();
-
-		if(!processadorExecutando) {
-			escalonarProcesso();
-		}
-
+	while(true){
+		updateBlockedProcesses();		
+		criaProcessos();
+		processador();		
 		atualizarTempoEsperaProcessosReady();
-
-		tempoDecorrido = tempoDecorrido + 1;
+		tempoDecorrido++;
 	}
 }
 
+/*-Functions----------------------------------------------------------*/
 void inicializacao() {
 	initSrand();
 	initFilas();
 	tempoDecorrido = 0;
 	tempoExecutando = 0;
-	proximoProcessoASerCriado = 0;
-	processadorExecutando = 0;
+	numProcesso = 0;
 	tempoExecutadoProcessador = 0;
+	processoExecutando = NULL;
 }
 
+/*Processador escalona um novo processo em 2 casos
+ *1) Se não houver processo escalonado atualmente
+ *2) Se o processo atualmente escalonado fizer um IO no tempo atual*/
 void executarProcesso() {
-	pedirIO(processoExecutando, tempoDecorrido);
-	processoExecutando->tempoExecutado++;
-	tempoExecutadoProcessador++;
+	if(!processoExecutando) escalonarProcesso();
+	else if(pedirIO(processoExecutando, tempoDecorrido)) escalonarProcesso();
+	if(processoExecutando) {
+		processoExecutando->tempoExecutado++;
+		tempoExecutadoProcessador++;
+	}
 }
 
 void escalonarProcesso() {
-	processadorExecutando = 1;
 	tempoExecutadoProcessador = 0;
 	processoExecutando = selecionarProximoProcessoAExecutar();
 }
 
 void processador() {
-
 	if(tempoExecutadoProcessador > TEMPO_RR) {
 		interromperProcesso(processoExecutando);
-		processoExecutando = NULL;
-		processadorExecutando = 0;
+		escalonarProcesso();
 	}
-	else {
-		executarProcesso();
-	}
+	else executarProcesso();
 }
 
 void geraTempoAleatorioParaCriacaoProcessos() {
-	//TODO preencher o vetor tempoGeracaoProcessos
+	int i;
+	for(i=0;i<MAX_PROCESSOS;i++){
+		tempoGeracaoProcessos[i]= rand()%TEMPO_MAX_CHEGADA;
+	}
 }
 
-void criaProcessosSeHouverDemanda() {
-	if(tempoGeracaoProcessos[proximoProcessoASerCriado] == tempoDecorrido) {
-		adicionarProcessoNovo(createNewProcess(5, 0, tempoDecorrido));
-		proximoProcessoASerCriado++;
+void criaProcessos() {
+	if(numProcesso<MAX_PROCESSOS && 
+	  tempoGeracaoProcessos[numProcesso] == tempoDecorrido) {
+		adicionarProcessoNovo(createNewProcess((2+rand()%5), 0, tempoDecorrido));
+		numProcesso++;
 	}
 }
